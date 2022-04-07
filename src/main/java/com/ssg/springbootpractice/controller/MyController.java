0b52ee76c64dea1e5b9bafe94d3cdf9d0400bef6
SoungWoo Park <parksoungwoo@ssg.com>
2022-04-07 14:15:42 +0900

    Change#2 - constructor to lombok

--- src/main/java/com/ssg/springbootpractice/controller/MyController.java
+++ src/main/java/com/ssg/springbootpractice/controller/MyController.java
@@ -1,10 +1,12 @@
 package com.ssg.springbootpractice.controller;
 
 import com.ssg.springbootpractice.service.MyService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@RequiredArgsConstructor
 @Controller
 public class MyController {
 
